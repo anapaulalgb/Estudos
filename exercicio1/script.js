52f739ab9013a5ba7ba34f67cfdf56dd8527f1cb
anapaulalgb <ana_paula7@outlook.com.br>
2024-11-04 15:59:52 -0300

    Atualizando index.html e script.js

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -3,20 +3,20 @@ function carregar (){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date ()
-    //var hora = data.getHours()
-    var hora = 22
+    var hora = data.getHours()
+    
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
       //BOM DIA
-      img.scr = 'manha.png'
+      img.scr = 'foto/manha.png'
       document.body.style.background = '#f4ebe1'  
     } else if (hora >= 12 && hora < 18) {
         //BOA TARDE
-        img.src = 'tarde.png'
+        img.src = 'foto/tarde.png'
         document.body.style.background = "#bbbac0"
     } else {
         //BOA NOITE
-        img.src = 'noite.png'
+        img.src = 'foto/noite.png'
         document.body.style.background = '#042e47'
     }
 }
